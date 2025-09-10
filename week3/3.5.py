baba36a30b22ec7032ebc9000f4a9e1acfe64e23
Eddoxian <aaronn.monk001@albright.edu>
2025-09-10 09:09:21 -0400

    first commit

---
+++ week3/3.5.py
@@ -0,0 +1,9 @@
+guest = ['lindsey', 'mark', 'bob']
+backup_list = ['Will', 'Tom', "Frank"]
+print(f"{guest[0]}, You're invited")
+print(f"{guest[2]}, You're invited")
+print(f"{guest[1]} couldnt make it so that means that")
+print(f"{backup_list[0]} is invited in his stead! So that means")
+print(f"{guest[0]}, You're invited")
+print(f"{guest[2]}, You're invited")
+print(f"{backup_list[0]}, You're invited")
---
+++ week3/3.8.py
@@ -0,0 +1,10 @@
+location = ["cyprus", "france", "egypt", "japan", "china"]
+original = location[:]
+print(location)
+alphabetical = sorted(location)
+print(alphabetical)
+print(original)
+location.reverse()
+print(location)
+location.reverse()
+print(location)
