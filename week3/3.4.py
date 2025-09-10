ffd05be3415f6f23669a552456496c2ed0f198be
Eddoxian <aaronn.monk001@albright.edu>
2025-09-10 09:09:58 -0400

    commit

---
+++ week3/3.4.py
@@ -0,0 +1,4 @@
+guest = ['lindsey', 'mark', 'bob']
+print(f"{guest[0]}, You're invited")
+print(f"{guest[2]}, You're invited")
+print(f"{guest[1]}, You're invited")
---
+++ week3/3.6.py
@@ -0,0 +1,13 @@
+guest = ['lindsey', 'mark', 'bob']
+backup_list = ['Will', 'Tom', "Frank"]
+backup_list.insert(0, "Domonic")
+backup_list.insert(3, "Rob")
+backup_list.append("Milly")
+print(f"{guest[0]}, You're invited")
+print(f"{guest[2]}, You're invited")
+print(f"{guest[1]} couldnt make it so that means that {backup_list[0]} is invited in his stead! So that means")
+print(f"{guest[0]}, You're invited")
+print(f"{guest[2]}, You're invited")
+print(f"{backup_list[0]}, We have a bigger table, You're invited")
+print(f"{backup_list[3]}, We have a bigger table, You're invited")
+print(f"{backup_list[5]}, We have a bigger table, You're invited")
