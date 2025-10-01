acd870500bfbcab0b805aa6231a0d3a1d9dcd7fc
Eddoxian <aaronn.monk001@albright.edu>
2025-10-01 08:18:10 -0400

    saved

---
+++ week6/6.1.py
@@ -0,0 +1,6 @@
+person1 = {"First_Name" : "Mike",
+           "Last_Name" : "Richardson",
+           "Age" : "47",
+           "City" : "New York"}
+for property in person1:
+    print(f"{property} ; {person1[property]}")
