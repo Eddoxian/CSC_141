4dbe663f63bfb88dee95eaab1197a1b32d541ed4
Eddoxian <aaronn.monk001@albright.edu>
2025-10-22 08:45:57 -0400

    saved

--- week8/8.5.py
+++ week8/8.5.py
@@ -1,1 +1,6 @@
-def 
+def cities(city, country="Japan"):
+    print(f"{city} is in {country}.")
+
+cities("osaka")
+cities("kyoto")
+cities("Atlanta", "USA")
---
+++ week8/8.6.py
@@ -0,0 +1,6 @@
+def cities(city, country):
+    return(f"{city}, {country}.")
+
+print (cities("osaka", "japan"))
+print (cities("kyoto", "japan"))
+print (cities("Atlanta", "USA"))
